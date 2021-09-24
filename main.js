6e934f0d04445607c6534e669f7ac3669f84fb72
Haardik123 <75983103+Haardik123@users.noreply.github.com>
2021-09-24 16:31:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+img="";
+status="";
+objects = []; 
+
+function setup(){
+    canvas = createCanvas(380 , 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380 , 380);
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocssd' , modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
+
+}
+
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+
+function draw(){
+    image(video , 0 , 0 , 380 , 380);
+
+    if (status != ""){
+        r = random(255); g = random(255); b = random(255);
+        objectDetector.detect(video , gotResult);
+        for(i = 0; i <= objects.length; i++){
+         document.getElementById("status").innerHTML = "Status: object detected";
+         document.getElementById("number_of_objects").innerHTML = "number of objects detected are: " + objects.length;
+         fill(r , g , b);
+         percent = floor(objects[i].confidence*100);
+         text(objects[i].label + " " + percent + "%" , objects[i].x+15 , objects[i].y+15);
+         noFill();
+         stroke(r , g , b);
+         rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+            
+        
+    }
+
+    fill("red");
+    text("dog" , 45 , 75);
+
+    noFill();
+
+    stroke("red");
+    rect( 30 , 60 , 450 , 350);
+
+    fill("red");
+    text("cat" , 320 , 120);
+
+    noFill();
+
+    stroke("red");
+    rect( 300 , 90 , 270 , 320);
+}
+
+function modelLoaded(){
+    console.log('Model has been Loaded');
+    status = true;
+    objectDetector.detect(video , gotResult);
+} 
+
+function gotResult(error , results){
+    if(error){
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+}
